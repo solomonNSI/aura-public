ac885b75be613ff58d125280172264df6041d4da
Suleyman Hanyyev <solomonNSI@users.noreply.github.com>
2023-05-27 18:30:43 +0300

    wip

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Link, animateScroll as scroll } from "react-scroll";
 import './App.css';
 import { saveAs } from 'file-saver';
+import aura from './images/aura.png';
+import team from './images/team.jpeg';
 
 const App = () => {
   const handleDownload = () => {
@@ -15,13 +17,28 @@ const App = () => {
           <p>aura</p>
         <a href="https://demo.aurapalette.app/">Try the Demo</a>
       </header>
-      <div className="gradientLine"></div>
-      
+      <div className="gradientLine" />
       <section className="appSection" id="about">
         <div className='title'>The Aura Palette: where words radiate with colors.</div>
         <div className="headerSection">
-            <div className="aboutText">
+            <div className="intro">
+                In a flourishing world of designers, where creativity knows no bounds, 
+                Aura Palette emerges as the ultimate companion. With an abundance of 
+                creative minds seeking exceptional designs, our innovative tool harnesses the 
+                power of modern ML technology to inspire and elevate your artistic journey.
+                 Discover captivating color palettes, unlock hidden depths in your designs, 
+                 and make your mark in the realm where language and art unite. 
+                 Aura Palette: Empowering designers to create extraordinary masterpieces.
+            </div>
+            <div>
+              <img src={aura} alt="Image" className="image" />
+            </div>
+        </div>
+      </section>
 
+      <section className="appSection" id="about">
+        <div className="headerSection">
+            <div className="aboutText">
                 <div className='flexbox-right'>           
                     <div className='textSection'>
                         <div className="bulletPoint">Experience the Marvels of Language and Art</div>
@@ -29,9 +46,6 @@ const App = () => {
                     </div>
                     <div className='colorStrip1'/>
                 </div>
-
-
-
                 <div className='flexbox'>         
                     <div className='colorStrip2'/>  
                     <div className='textSection'>
@@ -39,9 +53,6 @@ const App = () => {
                         We introduce a revolutionary tool that breathes life into words, transforming them into vibrant and captivating colors. Powered by cutting-edge artificial intelligence and natural language processing, The Aura Palette invites creative professionals, writers, designers, and artists of all kinds to effortlessly translate their literary masterpieces, brand identities, or even abstract thoughts into stunning color palettes.
                     </div> 
                 </div>
-
-
-
                 <div className='flexbox-right'>           
                     <div className='textSection'>
                         <div className="bulletPoint">Beyond Conventional Tools</div>
@@ -49,9 +60,6 @@ const App = () => {
                     </div>  
                     <div className='colorStrip3'/>
                 </div>
-
-
-
                 <div className='flexbox'>         
                     <div className='colorStrip4'/>  
                     <div className='textSection'>   
@@ -59,80 +67,85 @@ const App = () => {
                         Embark on an awe-inspiring journey where words and colors unite to shape extraordinary narratives. Join us in this dynamic fusion of art and technology, where the boundaries of creativity are transcended.<br/><br/>
                     </div> 
                 </div>
-
-
-                <div className="bulletPointFinal">Unleash your creativity, unleash the power of The Aura Palette!</div>
             </div>
-            <a href="https://demo.aurapalette.app/">Try the Demo</a>
         </div>
     
-      </section>
-
-      <section className="appSection" id="details">
-        <h1>Project Details</h1>
-        <p className="description">Bilkent University<br/>Academic Year 2022-2023<br/>CS491/492 Senior Project<br/></p>
-        <p className="description"><span>Group Code:</span> T2313<br/></p>
-        <p className="description"><span>Supervisor: </span>Fazlı Can<br/></p>
-        <p className="description"><span>Instructors:</span> Erhan Dolak and Tağmaç Topal<br/></p>
       </section>
 
       <section className="appSection" id="team">
         <h1>Meet Our Team</h1>
         <p className = "description">This is Team Aura. Knowing each other, staying in communication, and helping each other made us a good team. Periodic meetings and roadmaps helped us achieve our goals.</p>
+        <img src={team} alt="Image" className="image"/>
+        <p style={{fontStyle: 'italic'}}> from left to right: </p>
         <div className="members">
           <div className="member">
-            <h2 className="name">Ata Seren</h2>
-            <p className="position">Back-end Developer</p>
-            <p className="work">Our coding maestro behind the scenes, ensuring flawless back-end functionality.</p>
+            <h2 className="name">Suleyman Hanyyev</h2>
+            <p className="position">Team Lead & ML Engineer</p>
+            <p className="work">Our dynamic R&D engineer, guiding success with expertise in ML engineering.</p>
+            <a href="https://auraslogbook.vercel.app/logbooks/solomon">Logbook</a>
           </div>
           <div className="member">
             <h2 className="name">Ayda Yurtoğlu</h2>
             <p className="position">Front-end Developer & Project Manager</p>
             <p className="work">The creative powerhouse behind color algorithms and seamless project management.</p>
-          </div>
-          <div className="member">
-            <h2 className="name">Can Avşar</h2>
-            <p className="position">Front-end Developer & UI/UX Designer</p>
-            <p className="work">The artistic mind shaping stunning UI/UX designs for an immersive experience.</p>
-          </div>
-          <div className="member">
-            <h2 className="name">Suleyman Hanyyev</h2>
-            <p className="position">Team Lead & ML Engineer</p>
-            <p className="work">Our dynamic R&D engineer, guiding success with expertise in ML engineering.</p>
+            <a href="https://auraslogbook.vercel.app/logbooks/ayda">Logbook</a>
           </div>
           <div className="member">
             <h2 className="name">Zeynepnur Cavcar</h2>
             <p className="position">Back-end Developer</p>
             <p className="work">The technical wizard ensuring flawless functionality in our powerful back-end.</p>
+            <a href="https://auraslogbook.vercel.app/logbooks/zeynep">Logbook</a>
+          </div>
+          <div className="member">
+            <h2 className="name">Can Avşar</h2>
+            <p className="position">Front-end Developer & UI/UX Designer</p>
+            <p className="work">The artistic mind shaping stunning UI/UX designs for an immersive experience.</p>
+            <a href="https://auraslogbook.vercel.app/logbooks/canavsar">Logbook</a> 
+          </div>
+          <div className="member">
+            <h2 className="name">Ata Seren</h2>
+            <p className="position">Back-end Developer</p>
+            <p className="work">Our coding maestro behind the scenes, ensuring flawless back-end functionality.</p>
+            <a href="https://auraslogbook.vercel.app/logbooks/ata">Logbook</a>
           </div>
         </div>
       </section>
-      
-      <section className="appSection" id="reports">
-        <h1>Reports</h1>
-        <p className="description">We have created reports throughout the term that explain our analysis, requirements, and detailed design. To access our reports, you can click the corresponding buttons below.</p>
-        <a href="https://docs.google.com/document/d/1_gahyqeTS_7V4IgEQfHVB7ISDBhLCeAkJ4_P90mJknk/">Project Specifications Report (Google Drive)</a>
-        <a href="https://docs.google.com/document/d/14P-eXy6u3TxRiCrVGTzCAYaOqO5kyKenyLHbgcYC9II/">Analysis and Requirement Report (Google Drive)</a>
-        <a href="https://docs.google.com/document/d/19Di1V2xKOsz6DGKa9PNGr4RFw3Jnh7qTCEuLtEDQXw8/">Detailed Design Report (Google Drive)</a>
+
+      <section className="appSectionReports" id="reports">
+        <div className="description">
+          <h1>Reports</h1>
+          <p className='text'>To access our reports, you can click the corresponding buttons below.</p>
+          <a href="https://docs.google.com/document/d/1_gahyqeTS_7V4IgEQfHVB7ISDBhLCeAkJ4_P90mJknk/">Project Specifications</a>
+          <a href="https://docs.google.com/document/d/14P-eXy6u3TxRiCrVGTzCAYaOqO5kyKenyLHbgcYC9II/">Analysis and Requirement</a>
+          <a href="https://docs.google.com/document/d/19Di1V2xKOsz6DGKa9PNGr4RFw3Jnh7qTCEuLtEDQXw8/">Detailed Design Report</a>
+          <a href="https://drive.google.com/file/d/1D36MpugBn7tlrmdADDDf1Ihaei_s6UgQ/view?usp=sharing">Final Report</a>
+        </div>
+        <div className="description">
+          <h1 className='finaltitle'>Final Presentation</h1>
+          <p className='text'>Explore our final presentation as you navigate through the slides, accessible via the button below.</p>
+          <a  href="https://drive.google.com/file/d/1djb06fWtRXYYMvjP8cFqal9HDINvRZUn/view?usp=sharing">View our Presentation</a>
+        </div>
       </section>
 
-      <section className="appSection" id="logbook">
-        <h1>Final Presentation</h1>
-        <p className="description">Explore our final presentation as you navigate through the slides, accessible via the button below.</p>
-        <a href="https://drive.google.com/file/d/1djb06fWtRXYYMvjP8cFqal9HDINvRZUn/view?usp=sharing">View our Presentation</a>
-      </section>
-      
-      <section className="appSection" id="logbook">
-        <h1>Logbook</h1>
-        <p className="description">We have logbooks throughout the term that explain what we have done on the project. To access our logbooks, you can click the button below. </p>
-        <a href="https://auraslogbook.vercel.app/logbooksall">Access our Logbooks</a>
-      </section>
 
-      <section className="appSection" id="demo">
-        <h1>Try the Application</h1>
-        <p className='description'>We invite you to explore our app and unleash your creativity by generating palettes with our advanced AI tool. If you have any feedback, we encourage you to utilize our feedback tool by clicking on the feedback button located on the top navigation bar of the app.</p>
+      <section className="appSection" id="about">
+
+        <div className="bulletPointFinal">Unleash your creativity, unleash the power of The Aura Palette!</div>
         <a href="https://demo.aurapalette.app/">Try the Demo</a>
       </section>
+
+
+      <div className="gradientLine2" />
+      <footer className="App-footer">
+        <div className='info'>
+          <p><span>Group Code:</span> T2313<br/></p>
+          <p><span>Supervisor: </span>Fazlı Can<br/></p>
+          <p><span>Instructors:</span> Erhan Dolak and Tağmaç Topal<br/></p>
+        </div>
+        <div style={{paddingRight: "50px"}}>
+          <p>© 2023 The Aura Palette</p>
+        </div>
+      </footer>
 
     </div>
   );
